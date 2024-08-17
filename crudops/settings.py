5878329449292c30dc4f3ff2fd650dfdeee324b3
MoranLeven <49155163+MoranLeven@users.noreply.github.com>
2024-08-17 06:37:46 +0530

    Update settings.py

--- crudops/settings.py
+++ crudops/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-database_url = os.environ.get()
+database_url = os.environ.get("DATABASE_URL")
 DATABASES["default"]  = dj_database_url.parse(database_url)
 # DATABASES = {
 #     'default': {
